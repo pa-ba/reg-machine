69eaa19b8796b55202f9e4fc4efef85e556c00ea
Patrick Bahr <patrick.bahr@gmail.com>
2018-06-14 16:38:23 +0200

    calculation for exceptions with two continuations

--- partial/ExceptionTwo.v
+++ partial/ExceptionTwo.v
@@ -1,4 +1,5 @@
-(** Calculation of the simple arithmetic language with exceptions. *)
+(** Calculation of the simple arithmetic language with exceptions
+using two code continuations. *)
 
 Require Import List.
 Require Import Tactics.
@@ -47,43 +48,35 @@ Inductive Code : Set :=
 | MARK : adr -> Code -> Code -> Code
 | HALT : Code.
 
-Fixpoint comp' (x : Expr) (r : adr) (c : Code) : Code :=
+Fixpoint comp' (x : Expr) (r : adr) (c : Code) (f : Code) : Code :=
   match x with
   | Val n => LOAD n c
-  | Add e1 e2 => comp' e1 r (STORE r (comp' e2 (next r) (ADD r c)))
-  | Throw => THROW
-  | Catch e1 e2 => MARK r (comp' e2 r c) (comp' e1 (next r) (UNMARK c))
+  | Add e1 e2 => comp' e1 r (STORE r (comp' e2 (next r) (ADD r c) f)) f
+  | Throw => LOAD 0 f
+  | Catch e1 e2 => comp' e1 r c (comp' e2  r c f)
   end.
 
-Definition comp (x : Expr) : Code := comp' x adr0 HALT.
+Definition comp (x : Expr) : Code := comp' x adr0 HALT HALT.
 
-Definition Han := option adr.
 
 Inductive Elem : Set :=
-| NUM : nat -> Elem
-| HAN : Code -> Han -> Elem.
+| NUM : nat -> Elem.
 
 (** * Virtual Machine *)
 
 Inductive Conf' : Type :=
-| conf : Code -> nat -> Han -> Conf'
-| fail : Han -> Conf'.
+| conf : Code -> nat -> Conf'.
 
 Definition Conf : Type := Conf' * Mem Elem.
 
-Notation "⟨ x , y , z , s ⟩" := (conf x y z, s).
-Notation "⟪ x , s ⟫" := (fail x, s).
+Notation "⟨ x , y , s ⟩" := (conf x y, s).
+
 
 Reserved Notation "x ==> y" (at level 80, no associativity).
 Inductive VM : Conf -> Conf -> Prop :=
-| vm_load n a c s p : ⟨LOAD n c, a , p, s⟩ ==> ⟨c , n, p, s⟩ 
-| vm_add c s a r n p : s[r]=  NUM n -> ⟨ADD r c, a , p, s⟩ ==> ⟨c , n + a, p, s⟩
-| vm_store c s a r p : ⟨STORE r c, a, p, s⟩ ==> ⟨c , a, p, s[r:=NUM a]⟩
-| vm_throw a s p : ⟨THROW, a, p, s⟩ ==> ⟪p, s⟫
-| vm_fail p p' s c : s[p] = HAN c p' -> ⟪ Some p, s⟫ ==> ⟨c, 0, p', s⟩
-| vm_unmark p p' s a c c' : s[p] = HAN c' p' ->
-                            ⟨UNMARK c, a, Some p, s⟩ ==> ⟨c, a, p', s⟩
-| vm_mark p r s a c c' : ⟨MARK r c' c, a, p, s⟩ ==> ⟨c, a, Some r, s[r:= HAN c' p]⟩
+| vm_load n a c s : ⟨LOAD n c, a , s⟩ ==> ⟨c , n, s⟩ 
+| vm_add c s a r n : s[r]=  NUM n -> ⟨ADD r c, a , s⟩ ==> ⟨c , n + a, s⟩
+| vm_store c s a r : ⟨STORE r c, a, s⟩ ==> ⟨c , a, s[r:=NUM a]⟩
 where "x ==> y" := (VM x y).
 
 Inductive cle : Conf -> Conf -> Prop :=
@@ -113,13 +106,13 @@ Import VMCalc.
 
 (** Specification of the compiler *)
 
-Theorem spec e r c a p s :
+Theorem spec e r c a f s :
   freeFrom r s ->
-  ⟨comp' e r c, a, p, s⟩
+  ⟨comp' e r c f, a, s⟩
     =|>
     match eval e with
-    | Some n => ⟨c , n, p, s⟩
-    | None => ⟪p, s⟫
+    | Some n => ⟨c , n, s⟩
+    | None => ⟨f , 0, s⟩
     end.
 
 
@@ -131,7 +124,7 @@ Proof.
   generalize dependent s.
   generalize dependent r.
   generalize dependent a.
-  generalize dependent p.
+  generalize dependent f.
   induction e;intros.
 
 (** Calculation of the compiler *)
@@ -140,117 +133,104 @@ Proof.
 
   begin
     match eval (Val n) with
-    | Some n' => ⟨c , n' ,p, s⟩
-    | None => ⟪p, s⟫
+    | Some n' => ⟨c , n', s⟩
+    | None => ⟨c , 0, s⟩
     end.
   = {auto}
-      ⟨ c, n, p, s⟩.
+      ⟨ c, n, s⟩.
   <== {apply vm_load}
-      ⟨ LOAD n c, a, p, s ⟩.
+      ⟨ LOAD n c, a, s ⟩.
   [].
 
 (** - [x = Add x1 x2]: *)
   
   begin
     match eval (Add e1 e2) with
-    | Some n => ⟨c , n ,p, s⟩
-    | None => ⟪p, s⟫
+    | Some n => ⟨c , n, s⟩
+    | None => ⟨f , 0, s⟩
     end.
   = { auto }
     match eval e1 with
     | Some n => match eval e2 with
-                | Some n' => ⟨c , n + n' ,p, s⟩
-                | None => ⟪p, s⟫
+                | Some n' => ⟨c , n + n' ,s⟩
+                | None => ⟨f , 0 ,s⟩
                 end
-    | None => ⟪p, s⟫
+    | None => ⟨f , 0 ,s⟩
     end.
   ≤ { auto }
     match eval e1 with
     | Some n => match eval e2 with
-                | Some n' => ⟨c , n + n' ,p, s[r:=NUM n]⟩
-                | None => ⟪p, s[r:=NUM n]⟫
+                | Some n' => ⟨c , n + n' ,s[r:=NUM n]⟩
+                | None => ⟨f , 0 ,s⟩
                 end
-    | None => ⟪p, s⟫
+    | None => ⟨f , 0 ,s⟩
     end.
   <== { apply vm_add }
       match eval e1 with
       | Some n => match eval e2 with
-                  | Some n' => ⟨ADD r c , n' ,p, s[r:=NUM n]⟩
-                  | None => ⟪p, s[r:=NUM n]⟫
+                  | Some n' => ⟨ADD r c , n', s[r:=NUM n]⟩
+                  | None => ⟨f , 0 ,s⟩
                   end
-      | None => ⟪p, s⟫
+      | None => ⟨f , 0 ,s⟩
       end.
+  ≤ { auto }
+    match eval e1 with
+    | Some n => match eval e2 with
+                | Some n' => ⟨ADD r c , n', s[r:=NUM n]⟩
+                | None => ⟨f , 0 ,s[r:=NUM n]⟩
+                end
+    | None => ⟨f , 0 ,s⟩
+    end.
   <|= { apply IHe2 }
       match eval e1 with
-      | Some n => ⟨comp' e2 (next r) (ADD r c) , n ,p, s[r:=NUM n]⟩
-      | None => ⟪p, s⟫
+      | Some n => ⟨comp' e2 (next r) (ADD r c) f , n, s[r:=NUM n]⟩
+      | None => ⟨f , 0 ,s⟩
       end.
   <== { apply vm_store }
       match eval e1 with
-      | Some n => ⟨STORE r (comp' e2 (next r) (ADD r c)) , n ,p, s⟩
-      | None => ⟪p, s⟫
+      | Some n => ⟨STORE r (comp' e2 (next r) (ADD r c) f) , n , s⟩
+      | None => ⟨f , 0 ,s⟩
       end.
   <|= { apply IHe1 }
-    ⟨comp' e1 r (STORE r (comp' e2 (next r) (ADD r c))), a,p, s⟩.
+    ⟨comp' e1 r (STORE r (comp' e2 (next r) (ADD r c) f)) f, a, s⟩.
   [].
 
 (** - [x = Throw]: *)
   
   begin
     match eval Throw with
-    | Some n => ⟨c , n ,p, s⟩
-    | None => ⟪p, s⟫
+    | Some n => ⟨c , n, s⟩
+    | None => ⟨f , 0 ,s⟩
     end.
   = { auto }
-      ⟪p, s⟫.
-  <== { apply vm_throw }
-      ⟨THROW, a, p, s⟩.
+      ⟨f , 0 ,s⟩.
+  <== { apply vm_load }
+      ⟨LOAD 0 f , a ,s⟩.
   [].
 
 (** - [x = Catch x1 x2]: *)
   
   begin
     match eval (Catch e1 e2) with
-    | Some n => ⟨c , n ,p, s⟩
-    | None => ⟪p, s⟫
+    | Some n => ⟨c , n ,s⟩
+    | None => ⟨f , 0 ,s⟩
     end.
   = { auto }
       match eval e1 with
-      | Some n => ⟨c , n ,p, s⟩
+      | Some n => ⟨c , n ,s⟩
       | None => match eval e2 with
-                | Some n' => ⟨c , n' ,p, s⟩
-                | None => ⟪p, s⟫
+                | Some n' => ⟨c , n' , s⟩
+                | None => ⟨f , 0 ,s⟩
                 end
       end.
   <|= { apply IHe2 }
       match eval e1 with
-      | Some n => ⟨c , n ,p, s⟩
-      | None => ⟨comp' e2  r c , 0 ,p, s⟩
+      | Some n => ⟨c , n, s⟩
+      | None => ⟨comp' e2  r c f , 0 ,s⟩
       end.
-  ≤ { eauto }
-      match eval e1 with
-      | Some n => ⟨c , n ,p, s⟩
-      | None => ⟨comp' e2  r c , 0 ,p, s[r:= HAN (comp' e2  r c) p]⟩
-      end.
-  <== {apply vm_fail}
-      match eval e1 with
-      | Some n => ⟨c , n ,p, s⟩
-      | None => ⟪Some r, s[r:= HAN (comp' e2  r c) p]⟫
-      end.
-  ≤ {auto}
-      match eval e1 with
-      | Some n => ⟨c , n ,p, s[r:= HAN (comp' e2  r c) p]⟩
-      | None => ⟪Some r, s[r:= HAN (comp' e2  r c) p]⟫
-      end.
-  <== {eapply vm_unmark}
-      match eval e1 with
-      | Some n => ⟨UNMARK c , n ,Some r, s[r:= HAN (comp' e2  r c) p]⟩
-      | None => ⟪Some r, s[r:= HAN (comp' e2  r c) p]⟫
-      end.
+
   <|= {apply IHe1}
-      ⟨comp' e1 (next r) (UNMARK c) , a ,Some r, s[r:= HAN (comp' e2  r c) p]⟩.
-  <== {apply vm_mark}
-      ⟨MARK r (comp' e2  r c) (comp' e1 (next r) (UNMARK c)) , a ,p, s⟩.
+      ⟨comp' e1 r c (comp' e2  r c f) , a , s⟩.
   [].
 Qed.
 
@@ -264,14 +244,14 @@ Qed.
 
 Lemma determ_vm : determ VM.
   intros C C1 C2 V. induction V; intro V'; inversion V'; subst;eauto.
-  rewrite H in *; dist. rewrite H in H3. dist. rewrite H in H5. dist.
+  rewrite H in *; dist. 
 Qed.
 
 
-Theorem sound x a p s n C : freeFrom adr0 s -> eval x = Some n -> ⟨comp x, a, p, s⟩ =>>! C -> exists s', C = ⟨HALT, n,p, s'⟩.
+Theorem sound x a s n C : freeFrom adr0 s -> eval x = Some n -> ⟨comp x, a, s⟩ =>>! C -> exists s', C = ⟨HALT, n,s'⟩.
 Proof.
   intros F E M.
-  pose (spec x adr0 HALT a p s F). unfold Reach in *. repeat autodestruct.
+  pose (spec x adr0 HALT a HALT s F). unfold Reach in *. repeat autodestruct.
   pose (determ_trc determ_vm) as D.
   unfold determ in D. inversion H0. subst. eexists. eapply D. apply M.
   rewrite E in *.
